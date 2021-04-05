47a5e442fa06632fbca30997c4bb88247d4e94ed
Leung <charlottexprojects@gmail.com>
2021-04-05 12:33:28 +0800

    add LeanTween, 2D Sprite Editor, Animation

--- ARMiniProject/Assets/Script/ARManager.cs
+++ ARMiniProject/Assets/Script/ARManager.cs
@@ -1,22 +1,174 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class ARManager : MonoBehaviour
 {
+    Color transparent = new Color(1, 1, 1, 0);
+    Color transparent50 = new Color(1, 1, 1, 0.5f);
+    Coroutine CoroutineBack = null;
+    Coroutine CoroutineBody = null;
+    Coroutine CoroutineMessage = null;
+    Coroutine CoroutineCloseAction = null;
+
+    [Header("--- Back Group ---")]
     [SerializeField] GameObject backGroup = null;
+    [SerializeField] GameObject ringGround = null;
+    [SerializeField] List<GameObject> ring;
+
+    [Header("--- Body Group ---")]
     [SerializeField] GameObject bodyGroup = null;
 
-    // Start is called before the first frame update
+    [Header("--- Message Group ---")]
+    [SerializeField] GameObject messageBlock = null;
+    RectTransform messageBG = null;
+    TMP_Text messageText = null;
+    [Header("--- Action Group ---")]
+    [SerializeField] GameObject ActionBlock = null;
+    TMP_Text ActionText = null;
+    float backTime = 0.2f;
+    string debugString = "Debug:\n";
+
+    List<string> message = new List<string>
+    {
+        "Detected the MacBook.",
+        "This is the Component of MacBook.",
+    };
+
+    List<string> actionMessage = new List<string>
+    {
+        "Step 1 : Unscrew the screws ...",
+    };
+
     void Start()
     {
-        backGroup.setActive(true);
-        bodyGroup.setActive(false);
+        Initizalition();
     }
 
-    // Update is called once per frame
-    void Update()
+    void Initizalition()
     {
-        
+        for (int i = 0; i < ring.Count; i++)
+            ring[i].SetActive(false);
+
+        messageBG = messageBlock.transform.Find("BG").transform.GetComponent<RectTransform>();
+        messageText = messageBlock.transform.Find("Text").transform.GetComponent<TMP_Text>();
+        ActionText = ActionBlock.transform.Find("Text").transform.GetComponent<TMP_Text>();
+
+        LeanTween.color(messageBG, transparent, 0);
+        ActionBlock.SetActive(false);
+    }
+
+    public void DetectedBack()
+    {
+        if (CoroutineBack == null)
+        {
+            CoroutineBack = StartCoroutine(_detectedBack());
+        }
+
+        if (CoroutineMessage == null)
+        {
+            CoroutineMessage = StartCoroutine(ShowMessage(0));
+        }
+
+        if (CoroutineCloseAction != null)
+        {
+            StopCoroutine(CoroutineCloseAction);
+            CoroutineCloseAction = null;
+        }
+    }
+
+    public void LostBack()
+    {
+        if (CoroutineBack != null)
+        {
+            StopCoroutine(CoroutineBack);
+            CoroutineBack = null;
+
+            for (int i = 0; i < ring.Count; i++)
+                ring[i].SetActive(false);
+        }
+
+        if (CoroutineCloseAction == null)
+        {
+            CoroutineCloseAction = StartCoroutine(CloseAction());
+        }
+
+        if (CoroutineMessage != null)
+        {
+            StopCoroutine(CoroutineMessage);
+            CoroutineMessage = null;
+        }
+    }
+
+    public void DetectedBody()
+    {
+        if (CoroutineBody == null)
+        {
+            CoroutineBody = StartCoroutine(_detectedBody());
+        }
+
+        if (CoroutineMessage == null)
+        {
+            CoroutineMessage = StartCoroutine(ShowMessage(1));
+        }
+
+        if (CoroutineCloseAction != null)
+        {
+            StopCoroutine(CoroutineCloseAction);
+        }
+    }
+
+    IEnumerator _detectedBack()
+    {
+        ActionBlock.SetActive(true);
+        ActionText.text = actionMessage[0];
+
+        for (int i = 0; i < ring.Count; i++)
+        {
+            ring[i].SetActive(true);
+            yield return new WaitForSeconds(backTime);
+        }
+
+        backTime = 0.05f;
+    }
+
+    IEnumerator _detectedBody()
+    {
+        yield return new WaitForSeconds(0.2f);
+    }
+
+
+    IEnumerator ShowMessage(int stringNumber, float displayTime = 3)
+    {
+        messageText.color = Color.black;
+        messageText.text = message[stringNumber];
+        LeanTween.color(messageBG, transparent50, 0.5f);
+
+        yield return new WaitForSeconds(displayTime);
+
+        messageText.color = transparent;
+        LeanTween.color(messageBG, transparent, 2);
+    }
+
+    // After Lost Target 3 second will close Action Block
+    IEnumerator CloseAction()
+    {
+        yield return new WaitForSeconds(3);
+        backTime = 0.2f;
+        ActionBlock.SetActive(false);
+    }
+
+    void OnGUI()
+    {
+        if (Application.isEditor || Debug.isDebugBuild)
+        {
+            GUIStyle myStyle = new GUIStyle(GUI.skin.button);
+            myStyle.fontSize = 20;
+            myStyle.normal.textColor = Color.green;
+            myStyle.hover.textColor = Color.red;
+
+            GUI.Box(new Rect(10, 10, 200, 100), debugString, myStyle);
+        }
     }
 }
---
+++ ARMiniProject/Assets/Script/RingAnimation.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RingAnimation : MonoBehaviour
+{
+    Coroutine scale = null;
+    private void OnEnable()
+    {
+        if (scale == null)
+            scale = StartCoroutine(Animation());
+    }
+
+    private void OnDisable()
+    {
+        if (scale != null)
+        {
+            StopCoroutine(scale);
+            scale = null;
+        }
+    }
+
+    IEnumerator Animation()
+    {
+        while (true)
+        {
+            LeanTween.scale(gameObject, new Vector3(2, 2, 2), 3);
+            yield return new WaitForSeconds(3);
+
+            LeanTween.scale(gameObject, new Vector3(1, 1, 1), 3);
+            yield return new WaitForSeconds(3);
+        }
+    }
+}
